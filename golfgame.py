18351795788e3b597ef76ab7aa0529d173348f75
alban <alban.pascal91@gmail.com>
2024-01-27 23:47:28 +0100

    Adding ball deplacement with power and distance

--- golfgame.py
+++ golfgame.py
@@ -11,25 +11,54 @@ class Game:
 
         pygame.display.set_caption('JO 2024 Simulator')
         self.screen = pygame.display.set_mode((800, 600))
-        self.bg_img = pygame.image.load(os.path.join('spirit', 'background.png'))
-        self.hole_img = pygame.image.load(os.path.join('spirit', 'hole.png'))
+        self.bg_img = pygame.image.load(os.path.join('spirit', 'golfBackground.png'))
+        self.hole_img = pygame.image.load(os.path.join('spirit', 'golfHole.png'))
         self.clock = pygame.time.Clock()
         self.ball = golfgame_class.Ball(self, (300, 200), 0)
         self.arrow = golfgame_class.Arrow(self, self.ball.rect.center)
 
     def run(self):
-        while True:
+        running = True
+        taking_shoot = False
+        shoot_in_gestion = False
+        shoot_angle = 0
+        distance = 0
+        power = 1
+        while running:
             self.screen.blit(self.bg_img, (0, 0))
             self.screen.blit(self.ball.ballimg, self.ball.rect)
             self.screen.blit(self.hole_img, (0, 0))
+
+            if taking_shoot:
+                shoot_angle = math.degrees(math.atan2(self.ball.dist_mous_ball(pygame.mouse.get_pos())[1], self.ball.dist_mous_ball(pygame.mouse.get_pos())[0]))
+                self.arrow.rotate_angle(shoot_angle, self.ball.dist_mous_ball(pygame.mouse.get_pos())[2])
+                self.arrow.draw()
+                power = self.ball.dist_mous_ball(pygame.mouse.get_pos())[2]/40
+                distance = 100
+
+
+            if shoot_in_gestion and distance != 0:
+                taking_shoot = False
+                self.ball.update_pos(shoot_angle, power)
+                distance -= 1
+                power /= 1.005
+
+            if distance == 0:
+                distance = 100
+                shoot_in_gestion = False
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            if (pygame.mouse.get_pressed()[0] == True) and (self.ball.check_mouse_in_circledrag(pygame.mouse.get_pos())):
-                self.screen.blit(self.arrow.arrowimg, self.arrow.rect)
-                print(pygame.mouse.get_pos())
-            pygame.display.update()
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    taking_shoot = True
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    taking_shoot = False
+                    shoot_in_gestion = True
+
+            pygame.display.flip()
             self.clock.tick(60)
+        pygame.quit()
+        sys.exit()
 
 Game().run()
--- golfgame_class.py
+++ golfgame_class.py
@@ -4,46 +4,55 @@ import math
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, game, pos: list, velocity: int):
-        self.ballimg = pygame.image.load(os.path.join('spirit', 'ball.png'))
+        self.ballimg = pygame.image.load(os.path.join('spirit', 'golfBall.png'))
         self.rect = self.ballimg.get_rect()
         self.rect.center = pos
         self.game = game
-        self.velocity = 0
         self.victory_condition = False
         self.circledrag_area = math.pi * 100 ** 2
-
-    def get_pos(self):
-        return self.rect.center
-
-    def get_victory_condition(self):
-        return self.victory_condition
 
     def victory(self):
         self.victory_condition = True
         return self.victory_condition
 
-    def update_pos(self, change: list = [0,0]):
-        self.rect.center[0] += change[0]
-        self.rect.center[1] += change[1]
+    def update_pos(self,shoot_angle=0, power=1):
+        x = math.cos(math.radians(shoot_angle))
+        y = math.sin(math.radians(shoot_angle))
+        ball_pos = self.rect.center
+        self.rect.center = (ball_pos[0] - x*power, ball_pos[1] + y*power)
 
-    def check_mouse_in_circledrag(self, pos_mouse):
-        dist = [abs(self.rect.center[0]-pos_mouse[0]),abs(self.rect.center[1]-pos_mouse[1])]
-        dist_c = math.sqrt(dist[0]**2+dist[1]**2)
-        if dist_c <= 100:
+    def dist_mous_ball(self, pos_mouse):
+        dist = [abs(self.rect.center[0] - pos_mouse[0]), abs(self.rect.center[1] - pos_mouse[1])]
+        dist_c = math.sqrt(dist[0] ** 2 + dist[1] ** 2)
+        if dist_c > 150:
+            dist_c = 150
+        return -(self.rect.center[0]-pos_mouse[0]), self.rect.center[1]-pos_mouse[1], dist_c
+
+    def check_ball_stop(self, previous_ball_rect_center):
+        if previous_ball_rect_center == self.rect.center:
             return True
-        return False
+        else:
+            return False
+
 
 class Arrow(pygame.sprite.Sprite):
     def __init__(self, game, pos: list):
+        self.original_img = pygame.image.load(os.path.join('spirit', 'golfArrow.png')).convert_alpha()
         self.angle = 0
-        self.arrowimg = pygame.transform.rotate(pygame.image.load(os.path.join('spirit', 'arrow_direction.png')), self.angle)
-        self.rect = self.arrowimg.get_rect()
-        self.rect.midbottom = pos
+        self.rect = self.original_img.get_rect()
+        self.rect.center = pos
         self.game = game
-        self.circledrag_area = math.pi * 100 ** 2
 
-    def rotate(self, degree):
-        self.angle += degree
+    def rotate_angle(self, new_angle, dist_mous_ball):
+        self.angle = new_angle
+        arrowimg1 = pygame.transform.smoothscale_by(self.original_img, (dist_mous_ball/100, 0.5))
+        self.arrowimg  = pygame.transform.rotate(arrowimg1, self.angle)
+
+    def draw(self):
+        self.rect.center = self.game.ball.rect.center
+        self.game.screen.blit(self.arrowimg,
+                         (self.rect.centerx - self.arrowimg.get_width() / 2,
+                          self.rect.centery - self.arrowimg.get_height() / 2))
 
 class Hole:
     def __init__(self):
