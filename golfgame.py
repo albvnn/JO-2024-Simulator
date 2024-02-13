8bbea1fd9c8a5327dde520bb5763e1cc8fa8ffb8
alban <alban.pascal91@gmail.com>
2024-02-13 14:44:40 +0100

    Adding collision with barrier and river and response to environment (bounce or bring back to last pos)

--- golfgame.py
+++ golfgame.py
@@ -5,60 +5,109 @@ import sys
 import pymunk
 import pymunk.pygame_util
 import math
-class Game:
+class GolfGame:
     def __init__(self):
         pygame.init()
 
         pygame.display.set_caption('JO 2024 Simulator')
         self.screen = pygame.display.set_mode((800, 600))
         self.bg_img = pygame.image.load(os.path.join('spirit', 'golfBackground.png'))
-        self.hole_img = pygame.image.load(os.path.join('spirit', 'golfHole.png'))
         self.clock = pygame.time.Clock()
         self.ball = golfgame_class.Ball(self, (300, 200), 0)
         self.arrow = golfgame_class.Arrow(self, self.ball.rect.center)
+        self.hole = golfgame_class.Hole(self, (500, 50))
+        self.barrier = golfgame_class.Barrier(self, (0, 0))
+        self.river = golfgame_class.Water(self, (0,0))
+        pygame.mixer.init()
+        pygame.mixer.music.load(os.path.join('music', 'REQUIEM.mp3'))
+
+    def display_text(self, text, pos):
+        img = pygame.font.SysFont("Lato", 30).render(text, True, "WHITE")
+        self.screen.blit(img, (pos[0], pos[1]))
 
     def run(self):
         running = True
         taking_shoot = False
         shoot_in_gestion = False
+        termined_shoot = False
+        end_game = False
         shoot_angle = 0
         distance = 0
         power = 1
+        #pygame.mixer.music.play()
         while running:
             self.screen.blit(self.bg_img, (0, 0))
-            self.screen.blit(self.ball.ballimg, self.ball.rect)
-            self.screen.blit(self.hole_img, (0, 0))
+            if not self.hole.check_ball_in_hole(self.ball.rect.center):
+                self.screen.blit(self.ball.ballimg, self.ball.rect)
+            self.screen.blit(self.hole.holeimg, self.hole.rect)
+            self.barrier.wall_maker((40, 60), (180, 60))
+            self.barrier.wall_maker((180, 20), (180, 280))
+            self.river.river_maker((560, 40), (780, 40))
+
 
             if taking_shoot:
-                shoot_angle = math.degrees(math.atan2(self.ball.dist_mous_ball(pygame.mouse.get_pos())[1], self.ball.dist_mous_ball(pygame.mouse.get_pos())[0]))
-                self.arrow.rotate_angle(shoot_angle, self.ball.dist_mous_ball(pygame.mouse.get_pos())[2])
+                shoot_angle = -math.degrees(math.atan2(self.ball.dist_mous_ball(pygame.mouse.get_pos())[1], self.ball.dist_mous_ball(pygame.mouse.get_pos())[0]))
+                self.arrow.rotate_angle(-shoot_angle, self.ball.dist_mous_ball(pygame.mouse.get_pos())[2])
                 self.arrow.draw()
                 power = self.ball.dist_mous_ball(pygame.mouse.get_pos())[2]/40
                 distance = 100
 
+            for i in self.barrier.rectlist:
+                if pygame.Rect.colliderect(i[0], self.ball.rect):
+                    shoot_angle = self.ball.recalculate_shootangle_wall(i, shoot_angle)
+
+            for k in self.river.rectlist:
+                if pygame.Rect.colliderect(k[0], self.ball.rect):
+                    self.ball.rect.center = self.ball.last_pos
+                    distance = 0
 
             if shoot_in_gestion and distance != 0:
                 taking_shoot = False
-                self.ball.update_pos(shoot_angle, power)
+                termined_shoot = False
+                self.ball.update_pos(-shoot_angle, power)
                 distance -= 1
                 power /= 1.005
 
             if distance == 0:
                 distance = 100
+                self.ball.last_pos = self.ball.rect.center
+                shoot_in_gestion = False
+                termined_shoot = True
+                self.ball.counter_shot += 1
+
+            if self.hole.check_ball_in_hole(self.ball.rect.center):
+                distance = -1
+                self.display_text("VICTORY IN " + str(self.ball.counter_shot), (100, 100))
+                end_game = True
+                taking_shoot = False
                 shoot_in_gestion = False
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    taking_shoot = True
-                elif event.type == pygame.MOUSEBUTTONUP:
-                    taking_shoot = False
-                    shoot_in_gestion = True
+            if self.ball.counter_shot >= 10:
+                self.display_text("GAME OVER", (100, 100))
+                end_game = True
+
+
+            if not end_game and termined_shoot:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                        pygame.quit()
+                        sys.exit()
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        taking_shoot = True
+                    elif event.type == pygame.MOUSEBUTTONUP:
+                        taking_shoot = False
+                        shoot_in_gestion = True
+            else:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                        pygame.quit()
+                        sys.exit()
 
             pygame.display.flip()
             self.clock.tick(60)
-        pygame.quit()
-        sys.exit()
 
-Game().run()
+GolfGame().run()
+pygame.quit()
+sys.exit()
--- golfgame_class.py
+++ golfgame_class.py
@@ -7,9 +7,11 @@ class Ball(pygame.sprite.Sprite):
         self.ballimg = pygame.image.load(os.path.join('spirit', 'golfBall.png'))
         self.rect = self.ballimg.get_rect()
         self.rect.center = pos
+        self.last_pos = pos
         self.game = game
         self.victory_condition = False
         self.circledrag_area = math.pi * 100 ** 2
+        self.counter_shot = 0
 
     def victory(self):
         self.victory_condition = True
@@ -34,6 +36,12 @@ class Ball(pygame.sprite.Sprite):
         else:
             return False
 
+    def recalculate_shootangle_wall(self, wall: list, shootangle):
+        if wall[1] == "horizontal":
+            return -shootangle
+        elif wall[1] == "vertical":
+            return 180-shootangle
+
 
 class Arrow(pygame.sprite.Sprite):
     def __init__(self, game, pos: list):
@@ -54,14 +62,80 @@ class Arrow(pygame.sprite.Sprite):
                          (self.rect.centerx - self.arrowimg.get_width() / 2,
                           self.rect.centery - self.arrowimg.get_height() / 2))
 
-class Hole:
-    def __init__(self):
-        pass
+class Hole(pygame.sprite.Sprite):
+    def __init__(self, game, pos):
+        self.holeimg = pygame.image.load(os.path.join('spirit', 'golfHole.png'))
+        self.rect = self.holeimg.get_rect()
+        self.rect.center = pos
+        self.game = game
+        self.victory_condition = False
 
-class Barrier:
-    def __init__(self):
-        pass
+    def check_ball_in_hole(self, ball_pos):
+        dist = [abs(self.rect.center[0] - ball_pos[0]), abs(self.rect.center[1] - ball_pos[1])]
+        dist_c = math.sqrt(dist[0] ** 2 + dist[1] ** 2)
+        if dist_c < 10:
+            return True
+        return False
+
+
+class Barrier(pygame.sprite.Sprite):
+    def __init__(self, game, pos):
+        self.bricksimg = pygame.image.load(os.path.join('spirit', 'bricksTile.png'))
+        self.rect = self.bricksimg.get_rect()
+        self.rect.center = pos
+        self.game = game
+        self.rectlist = []
+
+    def wall_maker(self, pos_start: tuple, pos_end: tuple):
+        rectwall = pygame.Rect(0, 0, 0, 0)
+        if pos_start[1] == pos_end[1] and pos_start[0] != pos_end[0] and abs(pos_start[0] - pos_end[0]) % 20 == 0:
+            rectwall.center = (pos_start[0], pos_start[1])
+            rectwall.height = 20
+            rectwall.width = pos_end[0] - pos_start[0]
+            for i in range(pos_start[0], pos_end[0], 20):
+                self.game.screen.blit(self.bricksimg, (i, pos_start[1]))
+            if rectwall not in self.rectlist:
+                self.rectlist.append((rectwall, "horizontal"))
+            return True
+        elif pos_start[1] != pos_end[1] and pos_start[0] == pos_end[0] and abs(pos_start[1] - pos_end[1]) % 20 == 0:
+            rectwall.center = (pos_start[0], pos_start[1])
+            rectwall.height = abs(pos_start[1] - pos_end[1])
+            rectwall.width = 20
+            for i in range(pos_start[1], pos_end[1], 20):
+                self.game.screen.blit(pygame.transform.rotate(self.bricksimg, 90), (pos_start[0], i))
+            if rectwall not in self.rectlist:
+                self.rectlist.append((rectwall, "vertical"))
+            return True
+        else:
+            return False
 
 class Water:
-    def __init__(self):
-        pass
+    def __init__(self, game, pos):
+        self.watersimg = pygame.image.load(os.path.join('spirit', 'waterTile.png'))
+        self.rect = self.watersimg.get_rect()
+        self.rect.center = pos
+        self.game = game
+        self.rectlist = []
+
+    def river_maker(self, pos_start, pos_end):
+        rectriver = pygame.Rect(0, 0, 0, 0)
+        if pos_start[1] == pos_end[1] and pos_start[0] != pos_end[0] and abs(pos_start[0] - pos_end[0]) % 20 == 0:
+            rectriver.center = (pos_start[0], pos_start[1])
+            rectriver.height = 20
+            rectriver.width = pos_end[0] - pos_start[0]
+            for i in range(pos_start[0], pos_end[0], 20):
+                self.game.screen.blit(self.watersimg, (i, pos_start[1]))
+            if rectriver not in self.rectlist:
+                self.rectlist.append((rectriver, "horizontal"))
+            return True
+        elif pos_start[1] != pos_end[1] and pos_start[0] == pos_end[0] and abs(pos_start[1] - pos_end[1]) % 20 == 0:
+            rectriver.center = (pos_start[0], pos_start[1])
+            rectriver.height = abs(pos_start[1] - pos_end[1])
+            rectriver.width = 20
+            for i in range(pos_start[1], pos_end[1], 20):
+                self.game.screen.blit(pygame.transform.rotate(self.watersimg, 90), (pos_start[0], i))
+            if rectriver not in self.rectlist:
+                self.rectlist.append((rectriver, "vertical"))
+            return True
+        else:
+            return False
